ae5241ad9c34d71760d383f653adaca251eeb0db
Jacob Zimmer <jacobzimmer1@outlook.com>
2022-09-22 16:33:14 -0600

    feat: added dom updates

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-// var fighters = ['✄','🖖🏽','📑','🪨','🦎']
+
 
 var scissorFighter = document.querySelector('.scissors-fighter')
 var lizardFighter = document.querySelector('.lizard-fighter')
@@ -6,63 +6,57 @@ var rockFighter = document.querySelector('.rock-fighter')
 var paperFighter = document.querySelector('.paper-fighter')
 var spockFighter = document.querySelector('.spock-fighter')
 var fighterDisplay = document.querySelector('.fight-section')
+var resetButton = document.getElementById('#reset-button')
+var humanWins = document.querySelector('.human-win-count')
+var cpuWins = document.querySelector('.cpu-win-count')
 
-var fighters = [scissorFighter, lizardFighter, rockFighter, paperFighter, spockFighter]
-
-scissorFighter.addEventListener('click', humanChoseScissor)
-lizardFighter.addEventListener('click', humanChoseLizard)
-rockFighter.addEventListener('click', humanChoseRock)
-paperFighter.addEventListener('click', humanChosepaper)
-spockFighter.addEventListener('click', humanChoseSpock)
+var game = new Game()
 
 
+fighterDisplay.addEventListener('click', gameHandler)
 
-
-function getCpuFighter(fighters) {
-    var index = Math.floor(Math.random() * fighters.length);
-    return fighters[index].innerText; 
-  }
-
-  function hide(element) {
-    element.classList.add('hidden')
-   }
+  // function hide(element) {
+  //   element.classList.add('hidden')
+  //  }
    
-   function show(element) {
-   element.classList.remove('hidden')
+  //  function show(element) {
+  //  element.classList.remove('hidden')
+  //  }
+
+   function humanSelection(event) {
+    game.playerSelection(parseInt(event.target.id))
    }
 
-  function humanChoseScissor () {
-    hide(lizardFighter)
-    hide(rockFighter)
-    hide(paperFighter)
-    hide(spockFighter)
-    
+   function cpuSelection() {
+    game.getCpuFighter()
    }
 
-   function humanChoseLizard () {
-    hide(scissorFighter)
-    hide(rockFighter)
-    hide(paperFighter)
-    hide(spockFighter)
+   function gameHandler() {
+    humanSelection(event)
+    cpuSelection()
+    findWinner()
+    //reset dom function w/ reset class method
+    console.log('humanwins: ' + game.humanWinCount)
+    console.log('cpuwin: ' + game.cpuWinCount)
+    console.log('cpu: ' + game.cpuPick)
+    console.log('human: ' + game.humanPick)
    }
 
-   function humanChoseRock() {
-    hide(scissorFighter)
-    hide(lizardFighter)
-    hide(paperFighter)
-    hide(spockFighter)
+   function findWinner() {
+      game.declareWinner()
+      humanWins.innerHTML = `<p>${game.humanWinCount}</p>`
+      cpuWins.innerHTML = `<p>${game.cpuWinCount}</p>`
+      if(game.humanWinCount > 0) {
+        fighterDisplay.innerHTML = '<p>YOU WIN</p>'
+      }
+      else if (game.cpuWinCount > 0) {
+        fighterDisplay.innerHTML = '<p>YOU LOST</p>'
+      }
+      else {
+        fighterDisplay.innerHTML = '<p>TIE</p>'
+      }
    }
 
-   function humanChosepaper () {
-    hide(scissorFighter)
-    hide(lizardFighter)
-    hide(rockFighter)
-    hide(spockFighter)
-   }
+  
 
-   function humanChoseSpock () {
-    hide(scissorFighter)
-    hide(lizardFighter)
-    hide(paperFighter)
-    hide(rockFighter)
-   }
+ 
